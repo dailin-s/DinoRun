dc09d46ccf6d81cb9bb1513e8f97b990b9125c85
dailin <dailinsiggers@gmail.com>
2026-04-20 21:15:16 -0500

    jump feature added
    via dinobehaviour script

--- Assets/Assets/Scripts/DinoBehaviour.cs
+++ Assets/Assets/Scripts/DinoBehaviour.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DinoBehaviour : MonoBehaviour
 {
     public bool IS_ALIVE = true;
     private bool IS_GROUNDED;
     private Rigidbody2D dinoRigidbody; 
+    private float _jumpHeight = 10f;
+    private float _rayLength = 1.0f;
+    public LayerMask _groundLayer;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,10 +21,21 @@ public class DinoBehaviour : MonoBehaviour
         //check if rb is working
         //if (dinoRigidbody == null) {Debub.LogError["yo we're cooked rb dont work :/"];}
     }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.started && IS_GROUNDED)
+        {
+            dinoRigidbody.linearVelocity = new Vector2(dinoRigidbody.linearVelocityX, _jumpHeight);
+        }
+    }
  
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        RaycastHit2D playerRaycast = Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer);
+        IS_GROUNDED = playerRaycast.collider != null;
+
+
     }
 }
